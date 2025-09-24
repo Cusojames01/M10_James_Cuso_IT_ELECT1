4b42ad888ac20d8b73cbbf8d2bfb94c6419301a5
JamesCuso <cusojames@gmail.com>
2025-09-24 22:40:17 +0800

    main

---
+++ App.js
@@ -0,0 +1,30 @@
+import Counterapp from './Counterapp';
+
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+
+
+<Counterapp/>
+
+
+    
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
